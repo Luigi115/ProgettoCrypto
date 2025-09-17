5d9f73d9e8713edc4b300c3f654fccdcfe157ff7
Luigi Amato <jokerluigi@gmail.com>
2025-09-17 17:50:37 +0200

    aggiunta funzioni lettura blocchi, siamo arrivati al punto in cui abbiamo numero blocchi e tutti gli OP_RETURN uno dopo l'altro

--- utilityMinerMessage.py
+++ utilityMinerMessage.py
@@ -11,6 +11,52 @@ def estraiOpReturn(tx):
             op_returns.append(vout)
     return op_returns
 
+# Estrae tutti gli output OP_RETURN da una lista di transazioni, includendo info sul blocco
+def estraiOpReturnDaTransazioni(transazioni):
+    risultati = []
+
+    for tx in transazioni:
+        txid = tx.get("txid", "")
+        block_id = tx.get("block_id", "")
+        block_height = tx.get("block_height", "")
+        op_returns = []
+
+        for vout in tx.get("vout", []):
+            if vout.get("scriptpubkey_type", "").lower() == "op_return":
+                # prendiamo solo scriptpubkey_asm
+                op_returns.append(vout.get("scriptpubkey_asm", ""))
+
+        risultati.append({
+            "txid": txid,
+            "block_id": block_id,
+            "block_height": block_height,
+            "op_return": op_returns
+        })
+
+    return risultati
+
+# Restituisce solo il block_height e la lista degli OP_RETURN di ogni blocco.
+def filtraOpReturnPerBlocco(lista_blocchi):
+    risultati = []
+
+    for blocco in lista_blocchi:
+        blocco_filtrato = {
+            "block_height": blocco.get("block_height"),
+            "op_return": blocco.get("op_return", [])
+        }
+        risultati.append(blocco_filtrato)
+
+    return risultati
+
+#
+def stampaOpReturnPerBlocco(lista_blocchi):
+    for blocco in lista_blocchi:
+        print(f"Blocco numero: {blocco.get('block_height')}")
+        op_return_list = blocco.get("op_return", [])
+        for idx, op in enumerate(op_return_list, 1):
+            print(f"{idx}. {op}")
+        print("\n")  # Riga vuota tra i blocchi
+
  #Decodifica gli opreturn e li impacchetta in JSON
 def estraiOpReturnJSON(tx):
     risultati = {}
@@ -22,3 +68,4 @@ def estraiOpReturnJSON(tx):
         risultati[f"output {i}"] = msg
     
     return risultati
+
